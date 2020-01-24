1ccd5c93576c25e016a0a8a2101cd19f862f7ad0
dongji1111 <33479512+dongji1111@users.noreply.github.com>
2020-01-23 18:14:42 -0600

    Update train.py

--- train.py
+++ train.py
@@ -40,9 +40,11 @@ def main():
         for j in range(len(attack_eps_cifar[i])):
             attack_list.append([attack_name[i], attack_eps_cifar[i][j]])
     random.seed(1111)
-    attack_list = random.shuffle(attack_list)
+    random.shuffle(attack_list)
     attack_list_train = attack_list[0:16]
     attack_list_val = attack_list[16::]
+    print(attack_list_train)
+    print(attack_list_val)
     #print(len(attack_list))
     print(args)
 
@@ -92,24 +94,27 @@ def main():
                                          mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), ])
                                      )
 
-
     for epoch in range(args.epoch):
         # fetch meta_batchsz num of episode each time
         spt_size = args.n_way*args.k_spt
         qry_size = args.n_way*args.k_qry
         batch_size = spt_size+qry_size
         db = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=1, pin_memory=True)
+        attack_list_train = [attack_list_train[0]]
+        attack_list_val = [attack_list_val[0]]
+        accs, acc, loss =0,0,0
 
         for step, (x, y) in enumerate(db):
             x = x.to(device)
             y = y.to(device)
             x_spt, y_spt, x_qry, y_qry = [],[],[],[]
-            x_spt_clean, x_qry_clean = x[:50], x[50:200]
-            y_spt_clean, y_qry_clean = y[:50], y[50:200]
+            x_spt_clean, x_qry_clean = x[:spt_size], x[spt_size:batch_size]
+            y_spt_clean, y_qry_clean = y[:spt_size], y[spt_size:batch_size]
 
             for i in range(len(attack_list_train)):
                 x_spt_attack, x_qry_attack = x_spt_clean, x_qry_clean
                 # attack the picture
+                #print(attack_list_train[i][0],attack_list_train[i][1])
                 attack = get_attack('cifar-10', attack_list_train[i][0], attack_list_train[i][1], 10, 1, False)
                 attack = attack()
                 rand_target = torch.randint(0, 9, size=y_spt_clean.size(), device=device)
@@ -129,10 +134,58 @@ def main():
                 y_qry.append(y_qry_task.detach().numpy().tolist())
             x_spt, y_spt, x_qry, y_qry = torch.tensor(x_spt).to(device), torch.tensor(y_spt).to(device),\
                                          torch.tensor(x_qry).to(device), torch.tensor(y_qry).to(device)
-            accs, acc = maml(x_spt, y_spt, x_qry, y_qry)
 
-            if step % 1 == 0:
-                print('step:', step, '\ttraining acc:', acc)
+            accs, acc, loss = maml(x_spt, y_spt, x_qry, y_qry)
+            update_lr = maml.update_lr
+            meta_lr = maml.meta_lr
+            print('step:', step, 'update_lr', update_lr, 'meta_lr', meta_lr,
+                  '\ttraining acc:', acc, 'training loss:', loss)
+
+        if epoch % 1 == 0:
+
+            update_lr = maml.update_lr
+            meta_lr = maml.meta_lr
+            print('epoch:', epoch,'update_lr',update_lr,'meta_lr',meta_lr ,
+                  '\ttraining acc:', acc, 'training loss:', loss)
+            db_test = DataLoader(val_dataset, 1000, shuffle=True, num_workers=1, pin_memory=True)
+            # calculate test acc and loss
+            for step, (x, y) in enumerate(db_test):
+                if step >0:
+                    break
+                x = x.to(device)
+                y = y.to(device)
+                x_spt, y_spt, x_qry, y_qry = [], [], [], []
+                x_spt_clean, x_qry_clean = x[:2], x[2:20]
+                y_spt_clean, y_qry_clean = y[:2], y[2:20]
+                for i in range(len(attack_list_val)):
+                    x_spt_attack, x_qry_attack = x_spt_clean, x_qry_clean
+                    # attack the picture
+                    #print(attack_list_val[i][0], attack_list_val[i][1])
+                    attack = get_attack('cifar-10', attack_list_val[i][0], attack_list_val[i][1], 10, 1, False)
+                    attack = attack()
+                    rand_target = torch.randint(0, 9, size=y_spt_clean.size(), device=device)
+                    x_spt_attack = attack(maml.net, x_spt_clean, rand_target,
+                                          avoid_target=True, scale_eps=False)
+                    rand_target = torch.randint(0, 9, size=y_qry_clean.size(), device=device)
+                    x_qry_attack = attack(maml.net, x_qry_clean, rand_target,
+                                          avoid_target=True, scale_eps=False)
+                    x_spt_task = torch.cat([x_spt_clean, x_spt_attack], dim=0)
+                    x_qry_task = torch.cat([x_qry_clean, x_qry_attack], dim=0)
+                    y_spt_task = torch.cat([y_spt_clean, y_spt_clean], dim=0)
+                    y_qry_task = torch.cat([y_qry_clean, y_qry_clean], dim=0)
+                    # add it to the training data
+                    x_spt.append(x_spt_task.detach().numpy().tolist())
+                    x_qry.append(x_qry_task.detach().numpy().tolist())
+                    y_spt.append(y_spt_task.detach().numpy().tolist())
+                    y_qry.append(y_qry_task.detach().numpy().tolist())
+                x_spt, y_spt, x_qry, y_qry = torch.tensor(x_spt).to(device), torch.tensor(y_spt).to(device), \
+                                             torch.tensor(x_qry).to(device), torch.tensor(y_qry).to(device)
+                x_spt, y_spt, x_qry, y_qry = torch.squeeze(x_spt,0),torch.squeeze( y_spt,0),\
+                                             torch.squeeze( x_qry,0), torch.squeeze(y_qry,0)
+                accs, acc, loss = maml.finetunning(x_spt, y_spt, x_qry, y_qry)
+
+            print( '\ttesting acc:', acc, 'testing loss:', loss)
+
 
 
 if __name__ == '__main__':
@@ -143,7 +196,7 @@ if __name__ == '__main__':
     argparser.add_argument('--k_spt', type=int, help='k shot for support set', default=1)
     argparser.add_argument('--k_qry', type=int, help='k shot for query set', default=10)
     argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=4)
-    argparser.add_argument('--meta_lr', type=float, help='meta-level outer learning rate', default=1e-3)
+    argparser.add_argument('--meta_lr', type=float, help='meta-level outer learning rate', default=0.001)
     argparser.add_argument('--update_lr', type=float, help='task-level inner update learning rate', default=0.01)
     argparser.add_argument('--update_step', type=int, help='task-level inner update steps', default=5)
     argparser.add_argument('--update_step_test', type=int, help='update steps for finetunning', default=10)
